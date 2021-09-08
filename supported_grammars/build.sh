e64c6a5770b9e8733c888a16d1823ae94b746150
Ken Domino <kenneth.domino@domemtech.com>
2021-09-08 16:02:16 -0400

    Update build.

--- supported_grammars/build.sh
+++ supported_grammars/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cwd=`pwd`
-for i in abnf antlr2 antlr3 antlr4 bison cocor iso14977 javacc jison lark lbnf pegen pegjs pest rex sablecc w3cebnf xtext
+for i in abnf antlr2 antlr3 antlr4 bison cocor iso14977 javacc lark lbnf pegen pegjs pest rex w3cebnf xtext
 do
     echo $i
     cd $cwd/$i
@@ -11,14 +11,14 @@ do
         trgen --template-sources-directory ../templates > /dev/null 2>&1
 	if [ $? != 0 ]
 	then
-	    echo "Failed trgen"
+	    echo "Failed trgen of $i"
             rm -rf Generated
 	    continue;
 	fi
 	dotnet build Generated/*.csproj > /dev/null 2>&1
 	if [ $? != 0 ]
 	then
-	    echo "Failed build"
+	    echo "Failed build of $i"
             rm -rf Generated
 	    continue;
 	fi
