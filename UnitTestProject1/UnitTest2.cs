ab1915682d6746f139f8129f05b40e47ebdeae98
Ken Domino <kenneth.domino@domemtech.com>
2021-10-12 08:24:08 -0400

    Clean up some of the unit tests.

--- UnitTestProject1/UnitTest2.cs
+++ UnitTestProject1/UnitTest2.cs
@@ -1,5 +1,4 @@
-﻿using LanguageServer;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Linq;
 using Workspaces;
@@ -12,150 +11,65 @@ namespace UnitTestProject1
         [TestMethod]
         public void Kleene1()
         {
-            Workspace _workspace = new Workspace();
-            {
-                Document document = Document.CreateStringDocument(@"
+            var document = Setup.OpenAndParse(@"
 grammar kleene;
 a : | 'a' a;
 ");
-                _ = ParsingResultsFactory.Create(document);
-                var workspace = document.Workspace;
-                _ = new LanguageServer.Module().Compile(workspace);
-                Project project = _workspace.FindProject("Misc");
-                if (project == null)
-                {
-                    project = new Project("Misc", "Misc", "Misc");
-                    _workspace.AddChild(project);
-                }
-                project.AddDocument(document);
-                var pr = LanguageServer.ParsingResultsFactory.Create(document);
-                if (document.ParseTree == null)
-                {
-                    new LanguageServer.Module().Compile(_workspace);
-                }
-                var result = LanguageServer.Transform.ConvertRecursionToKleeneOperator(document);
-                if (!(result.Count == 1 && result.First().Value == @"
+            var result = LanguageServer.Transform.ConvertRecursionToKleeneOperator(document);
+            if (!(result.Count == 1 && result.First().Value == @"
 grammar kleene;
 a : 'a' * ;
 ")) throw new Exception();
-            }
         }
 
         [TestMethod]
         public void Kleene2()
         {
-            {
-                Workspace _workspace = new Workspace();
-                Document document = Document.CreateStringDocument(@"
+            Document document = Setup.OpenAndParse(@"
 grammar kleene;
 b : | b 'b';
 ");
-                _ = ParsingResultsFactory.Create(document);
-                var workspace = document.Workspace;
-                _ = new LanguageServer.Module().Compile(workspace);
-                Project project = _workspace.FindProject("Misc");
-                if (project == null)
-                {
-                    project = new Project("Misc", "Misc", "Misc");
-                    _workspace.AddChild(project);
-                }
-                project.AddDocument(document);
-                var pr = LanguageServer.ParsingResultsFactory.Create(document);
-                if (document.ParseTree == null)
-                {
-                    new LanguageServer.Module().Compile(_workspace);
-                }
-                var result = LanguageServer.Transform.ConvertRecursionToKleeneOperator(document);
-                if (!(result.Count == 1 && result.First().Value == @"
+            var result = LanguageServer.Transform.ConvertRecursionToKleeneOperator(document);
+            if (!(result.Count == 1 && result.First().Value == @"
 grammar kleene;
 b : 'b' * ;
 ")) throw new Exception();
-            }
         }
 
         [TestMethod]
         public void Kleene3()
         {
-            {
-                Workspace _workspace = new Workspace();
-                Document document = Document.CreateStringDocument(@"
+            var document = Setup.OpenAndParse(@"
 grammar kleene;
 xx  : xx yy | ;
 yy: 'b' ;
 ");
-                _ = ParsingResultsFactory.Create(document);
-                var workspace = document.Workspace;
-                _ = new LanguageServer.Module().Compile(workspace);
-                Project project = _workspace.FindProject("Misc");
-                if (project == null)
-                {
-                    project = new Project("Misc", "Misc", "Misc");
-                    _workspace.AddChild(project);
-                }
-                project.AddDocument(document);
-                var pr = LanguageServer.ParsingResultsFactory.Create(document);
-                if (document.ParseTree == null)
-                {
-                    new LanguageServer.Module().Compile(_workspace);
-                }
-                var result = LanguageServer.Transform.ConvertRecursionToKleeneOperator(document);
-                if (!(result.Count == 1 && result.First().Value == @"
+            var result = LanguageServer.Transform.ConvertRecursionToKleeneOperator(document);
+            if (!(result.Count == 1 && result.First().Value == @"
 grammar kleene;
 xx : yy * ;
 yy: 'b' ;
 ")) throw new Exception();
-            }
         }
 
         [TestMethod]
         public void Kleene4()
         {
-            {
-                Workspace _workspace = new Workspace();
-                Document document = Document.CreateStringDocument(@"
+            var document = Setup.OpenAndParse(@"
 grammar kleene;
 xx : 'a' xx | 'a';
 yy : yy 'b' | 'b' ;
 zz : | 'a' | 'a' zz;
 z2 : | 'b' | z2 'b';
 ");
-
-                // The Kleene rewrite currently produces
-                // xx : 'a' * 'a' ;
-                // yy: 'b' 'b' * ;
-                // zz: 'a' * ( | 'a');
-                // z2: ( | 'b') 'b' * ;
-                //
-                // This should be instead
-                // xx : 'a' + ;
-                // yy : 'b' + ;
-                // zz : 'a' * ;
-                // z2 : 'b' * ;
-
-                _ = ParsingResultsFactory.Create(document);
-                var workspace = document.Workspace;
-                _ = new LanguageServer.Module().Compile(workspace);
-                Project project = _workspace.FindProject("Misc");
-                if (project == null)
-                {
-                    project = new Project("Misc", "Misc", "Misc");
-                    _workspace.AddChild(project);
-                }
-                project.AddDocument(document);
-                var pr = LanguageServer.ParsingResultsFactory.Create(document);
-                if (document.ParseTree == null)
-                {
-                    new LanguageServer.Module().Compile(_workspace);
-                }
-                var result = LanguageServer.Transform.ConvertRecursionToKleeneOperator(document);
-                if (!(result.Count == 1 && result.First().Value == @"
+            var result = LanguageServer.Transform.ConvertRecursionToKleeneOperator(document);
+            if (!(result.Count == 1 && result.First().Value == @"
 grammar kleene;
 xx : 'a' + ;
 yy : 'b' + ;
 zz : 'a' * ;
 z2 : 'b' * ;
 ")) throw new Exception();
-            }
         }
     }
 }
--- UnitTestProject1/UnitTest3.cs
+++ UnitTestProject1/UnitTest3.cs
@@ -1,5 +1,4 @@
-﻿using LanguageServer;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Linq;
 using Workspaces;
@@ -12,69 +11,33 @@ namespace UnitTestProject1
         [TestMethod]
         public void Rup1()
         {
-            Workspace _workspace = new Workspace();
-            {
-                Document document = Document.CreateStringDocument(@"
+            Document document = Setup.OpenAndParse(@"
 grammar temp;
 foo: (bar)?; // <- can be safely replaced with `bar?`
 bar: 'baz';
 ");
-                _ = ParsingResultsFactory.Create(document);
-                var workspace = document.Workspace;
-                _ = new LanguageServer.Module().Compile(workspace);
-                Project project = _workspace.FindProject("Misc");
-                if (project == null)
-                {
-                    project = new Project("Misc", "Misc", "Misc");
-                    _workspace.AddChild(project);
-                }
-                project.AddDocument(document);
-                var pr = LanguageServer.ParsingResultsFactory.Create(document);
-                if (document.ParseTree == null)
-                {
-                    new LanguageServer.Module().Compile(_workspace);
-                }
-                var result = LanguageServer.Transform.RemoveUselessParentheses(document);
-                if (!(result.Count == 1 && result.First().Value == @"
+            var result = LanguageServer.Transform.RemoveUselessParentheses(document);
+            if (!(result.Count == 1 && result.First().Value == @"
 grammar temp;
 foo:bar?; // <- can be safely replaced with `bar?`
 bar: 'baz';
 ")) throw new Exception();
-            }
         }
 
         [TestMethod]
         public void Rup2()
         {
-            Workspace _workspace = new Workspace();
-            {
-                Document document = Document.CreateStringDocument(@"
+            Document document = Setup.OpenAndParse(@"
 grammar t2;
 assignment : ( ( ( ( '=>' ) ) | ( ( '->' ) ) ) ? ( ( validID ) ) ( ( ( '+=' | '=' | '?=' ) ) ) ( ( assignableTerminal ) ) ) ;
 // It should be assignment : ( '=>' | '->' ) ? validID ( '+=' | '=' | '?=' ) assignableTerminal ;
 ");
-                _ = ParsingResultsFactory.Create(document);
-                var workspace = document.Workspace;
-                _ = new LanguageServer.Module().Compile(workspace);
-                Project project = _workspace.FindProject("Misc");
-                if (project == null)
-                {
-                    project = new Project("Misc", "Misc", "Misc");
-                    _workspace.AddChild(project);
-                }
-                project.AddDocument(document);
-                var pr = LanguageServer.ParsingResultsFactory.Create(document);
-                if (document.ParseTree == null)
-                {
-                    new LanguageServer.Module().Compile(_workspace);
-                }
-                var result = LanguageServer.Transform.RemoveUselessParentheses(document);
-                if (!(result.Count == 1 && result.First().Value == @"
+            var result = LanguageServer.Transform.RemoveUselessParentheses(document);
+            if (!(result.Count == 1 && result.First().Value == @"
 grammar t2;
 assignment : ( '=>' | '->' ) ? validID ( '+=' | '=' | '?=' ) assignableTerminal ;
 // It should be assignment : ( '=>' | '->' ) ? validID ( '+=' | '=' | '?=' ) assignableTerminal ;
 ")) throw new Exception();
-            }
         }
     }
 }
