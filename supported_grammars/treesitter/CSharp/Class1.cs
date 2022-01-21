32adabbed9d2dc681196c47eb53eeed9f7513e43
Ken Domino <kenneth.domino@domemtech.com>
2022-01-21 09:46:21 -0500

    Add in readme and update code for conversion to ebnf.

--- supported_grammars/treesitter/CSharp/Class1.cs
+++ supported_grammars/treesitter/CSharp/Class1.cs
@@ -4,160 +4,157 @@ using Antlr4.Runtime.Tree;
 using org.eclipse.wst.xml.xpath2.processor.util;
 using System.Linq;
 
-namespace Test
+public class Class1 : JavaScriptParserBaseVisitor<object>
 {
-    public class Class1 : JavaScriptParserBaseVisitor<object>
+    public override object VisitSingleExpression([NotNull] JavaScriptParser.SingleExpressionContext context)
     {
-        public override object VisitSingleExpression([NotNull] JavaScriptParser.SingleExpressionContext context)
+        var name = context?.singleExpression()?.FirstOrDefault()?.identifier()?.Identifier()?.GetText();
+        var name2 = context?.singleExpression()?.FirstOrDefault()?.singleExpression()?.FirstOrDefault()?.identifier()?.Identifier()?.GetText();
+        if (name == "seq")
         {
-            var name = context?.singleExpression()?.FirstOrDefault()?.identifier()?.Identifier()?.GetText();
-            var name2 = context?.singleExpression()?.FirstOrDefault()?.singleExpression()?.FirstOrDefault()?.identifier()?.Identifier()?.GetText();
-            if (name == "seq")
+            var args = context.arguments().argument();
+            System.Console.Write("(");
+            foreach (var a in args)
             {
-                var args = context.arguments().argument();
-                System.Console.Write("(");
-                foreach (var a in args)
-                {
-                    Visit(a);
-                }
-                System.Console.Write(" )");
+                Visit(a);
             }
-            else if (name == "choice")
+            System.Console.Write(" )");
+        }
+        else if (name == "choice")
+        {
+            var args = context.arguments().argument();
+            System.Console.Write("(");
+            bool first = true;
+            foreach (var a in args)
             {
-                var args = context.arguments().argument();
-                System.Console.Write("(");
-                bool first = true;
-                foreach (var a in args)
-                {
-                    if (!first) System.Console.Write(" | ");
-                    first = false;
-                    Visit(a);
-                }
-                System.Console.Write(" )");
+                if (!first) System.Console.Write(" | ");
+                first = false;
+                Visit(a);
             }
-            else if (name == "repeat")
+            System.Console.Write(" )");
+        }
+        else if (name == "repeat")
+        {
+            var args = context.arguments().argument();
+            System.Console.Write("(");
+            foreach (var a in args)
             {
-                var args = context.arguments().argument();
-                System.Console.Write("(");
-                foreach (var a in args)
-                {
-                    Visit(a);
-                }
-                System.Console.Write(" )* ");
+                Visit(a);
             }
-            else if (name == "repeat1")
+            System.Console.Write(" )* ");
+        }
+        else if (name == "repeat1")
+        {
+            var args = context.arguments().argument();
+            System.Console.Write("(");
+            foreach (var a in args)
             {
-                var args = context.arguments().argument();
-                System.Console.Write("(");
-                foreach (var a in args)
-                {
-                    Visit(a);
-                }
-                System.Console.Write(" )+ ");
+                Visit(a);
             }
-            else if (name == "optional")
+            System.Console.Write(" )+ ");
+        }
+        else if (name == "optional")
+        {
+            var args = context.arguments().argument();
+            System.Console.Write("(");
+            foreach (var a in args)
             {
-                var args = context.arguments().argument();
-                System.Console.Write("(");
-                foreach (var a in args)
-                {
-                    Visit(a);
-                }
-                System.Console.Write(" )? ");
+                Visit(a);
             }
-            else if (name2 == "prec")
+            System.Console.Write(" )? ");
+        }
+        else if (name2 == "prec")
+        {
+            var args = context.arguments().argument();
+            System.Console.Write("(");
+            var count = args.Count();
+            var rest = args;
+            if (count == 0)
+            { }
+            else if (count == 1)
             {
-                var args = context.arguments().argument();
-                System.Console.Write("(");
-                var count = args.Count();
-                var rest = args;
-                if (count == 0)
-                { }
-                else if (count == 1)
-                {
-                    Visit(rest.First());
-                }
-                else
-                {
-                    foreach (var a in args.Skip(1))
-                    {
-                        Visit(a);
-                    }
-                }
-                System.Console.Write(" ) ");
+                Visit(rest.First());
             }
-            else if (name == "prec")
+            else
             {
-                var args = context.arguments().argument();
-                System.Console.Write("(");
                 foreach (var a in args.Skip(1))
                 {
                     Visit(a);
                 }
-                System.Console.Write(" ) ");
             }
-            else if (name == "token")
-            {
-                var args = context.arguments().argument();
-                System.Console.Write("(");
-                foreach (var a in args)
-                {
-                    Visit(a);
-                }
-                System.Console.Write(" ) ");
-            }
-            else if (name == "field")
-            {
-                var args = context.arguments().argument();
-                System.Console.Write("(");
-                foreach (var a in args)
-                {
-                    Visit(a);
-                }
-                System.Console.Write(" ) ");
-            }
-            else if (name == "$")
-            {
-                var nt = context.identifierName()?.identifier()?.Identifier()?.GetText();
-                System.Console.Write(" " + nt);
-            }
-            else if (context.literal() != null)
-            {
-                Visit(context.literal());
-            }
-            return null;
+            System.Console.Write(" ) ");
         }
-
-        public override object VisitLiteral([NotNull] JavaScriptParser.LiteralContext context)
+        else if (name == "prec")
         {
-            System.Console.Write(" " + context.GetText());
-            return Visit(context.GetChild(0));
-        }
-
-        public static void MyMain(IParseTree tree, Parser parser)
-        {
-            var visitor = new Test.Class1();
-            using (AntlrTreeEditing.AntlrDOM.AntlrDynamicContext dynamicContext = new AntlrTreeEditing.AntlrDOM.ConvertToDOM().Try(tree, parser))
+            var args = context.arguments().argument();
+            System.Console.Write("(");
+            foreach (var a in args.Skip(1))
             {
-                org.eclipse.wst.xml.xpath2.processor.Engine engine = new org.eclipse.wst.xml.xpath2.processor.Engine();
-                var nodes = engine.parseExpression(
-                    @"
+                Visit(a);
+            }
+            System.Console.Write(" ) ");
+        }
+        else if (name == "token")
+        {
+            var args = context.arguments().argument();
+            System.Console.Write("(");
+            foreach (var a in args)
+            {
+                Visit(a);
+            }
+            System.Console.Write(" ) ");
+        }
+        else if (name == "field")
+        {
+            var args = context.arguments().argument();
+            System.Console.Write("(");
+            foreach (var a in args)
+            {
+                Visit(a);
+            }
+            System.Console.Write(" ) ");
+        }
+        else if (name == "$")
+        {
+            var nt = context.identifierName()?.identifier()?.Identifier()?.GetText();
+            System.Console.Write(" " + nt);
+        }
+        else if (context.literal() != null)
+        {
+            Visit(context.literal());
+        }
+        return null;
+    }
+
+    public override object VisitLiteral([NotNull] JavaScriptParser.LiteralContext context)
+    {
+        System.Console.Write(" " + context.GetText());
+        return Visit(context.GetChild(0));
+    }
+
+    public static void MyMain(IParseTree tree, Parser parser)
+    {
+        var visitor = new Class1();
+        using (AntlrTreeEditing.AntlrDOM.AntlrDynamicContext dynamicContext = new AntlrTreeEditing.AntlrDOM.ConvertToDOM().Try(tree, parser))
+        {
+            org.eclipse.wst.xml.xpath2.processor.Engine engine = new org.eclipse.wst.xml.xpath2.processor.Engine();
+            var nodes = engine.parseExpression(
+                @"
 	//propertyAssignment[propertyName/identifierName/identifier/Identifier[text()='rules']]
 			    /singleExpression/objectLiteral/propertyAssignment
 			    ",
-                    new StaticContextBuilder()).evaluate(dynamicContext, new object[] { dynamicContext.Document })
-                    .Select(x => (x.NativeValue as AntlrTreeEditing.AntlrDOM.AntlrElement).AntlrIParseTree).ToList();
+                new StaticContextBuilder()).evaluate(dynamicContext, new object[] { dynamicContext.Document })
+                .Select(x => (x.NativeValue as AntlrTreeEditing.AntlrDOM.AntlrElement).AntlrIParseTree).ToList();
 
-                foreach (var r in nodes)
-                {
-                    var lhs = (r as JavaScriptParser.PropertyAssignmentContext)?.propertyName()?.identifierName()?.identifier()?.Identifier()?.GetText();
-                    var rhs = (r as JavaScriptParser.PropertyAssignmentContext)?
-                          .singleExpression()?.FirstOrDefault()?.anonymousFunction()?.arrowFunctionBody();
-                    System.Console.Write(lhs + " : ");
-                    visitor.Visit(rhs);
-                    System.Console.Write(" ;");
-                    System.Console.WriteLine();
-                }
+            foreach (var r in nodes)
+            {
+                var lhs = (r as JavaScriptParser.PropertyAssignmentContext)?.propertyName()?.identifierName()?.identifier()?.Identifier()?.GetText();
+                var rhs = (r as JavaScriptParser.PropertyAssignmentContext)?
+                      .singleExpression()?.FirstOrDefault()?.anonymousFunction()?.arrowFunctionBody();
+                System.Console.Write(lhs + " : ");
+                visitor.Visit(rhs);
+                System.Console.Write(" ;");
+                System.Console.WriteLine();
             }
         }
     }
