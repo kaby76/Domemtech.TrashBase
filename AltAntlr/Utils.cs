5aec3258291cce6ba7c0d39c9d97c1a64fd9b8ad
Ken Domino <kenneth.domino@domemtech.com>
2022-06-01 08:23:08 -0400

    Updates.

--- AltAntlr/Utils.cs
+++ AltAntlr/Utils.cs
@@ -1,38 +1,9 @@
-﻿namespace LanguageServer
+﻿namespace AltAntlr
 {
-    using Antlr4.Runtime;
-    using Antlr4.Runtime.Tree;
     using System.Collections.Generic;
-    using Workspaces;
 
     public class Util
     {
-
-        public static TerminalNodeImpl Find(int index, Document document)
-        {
-            ParsingResults pd = ParsingResultsFactory.Create(document);
-            var workspace = document.Workspace;
-            if (pd.ParseTree == null)
-            {
-                new Module().Compile(workspace);
-            }
-
-            foreach (IParseTree node in DFSVisitor.DFS(pd.ParseTree as ParserRuleContext))
-            {
-                if (node as TerminalNodeImpl == null)
-                {
-                    continue;
-                }
-
-                TerminalNodeImpl leaf = node as TerminalNodeImpl;
-                if (leaf.Symbol.StartIndex <= index && index <= leaf.Symbol.StopIndex)
-                {
-                    return leaf;
-                }
-            }
-            return null;
-        }
-
         static Dictionary<string, List<int>> _cache = new Dictionary<string, List<int>>();
         private static List<int> ComputeIndexes(string buffer)
         {
